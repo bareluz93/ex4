596d03e47261b42eef0e331ce3624c8127a83e09
bar eluz <bareluz93@gmail.com>
2018-01-18 12:50:23 +0200

    finish extractor still debugging

--- ex4.py
+++ ex4.py
@@ -1,50 +1,72 @@
 import wikipedia, spacy, copy
+
 nlp_model = spacy.load('en')
 page = wikipedia.page('Brad Pitt').content
 analyzed_page = nlp_model(page)
+
+
+# for token in analyzed_page:
+#     print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
+#           token.shape_, token.is_alpha, token.is_stop)
+
+
+class linear_relation_extractor:
+    last_token_was_propnoun = False
+
+    relations_triplets = []
+    prop_noun_set = set()
+    triplet = {'subject': [], 'relation': [], 'object': []}
+
+    def step(self, token):
+        if token.pos_ == "PROPN":
+            self.handle_propn(token)
+        else:
+            if self.last_token_was_propnoun:
+                self.finish_last_triplet_and_start_next()
+            if token.pos_ == "VERB" or token.pos_ == "ADP":
+                self.handle_verb_or_adp(token)
+            elif token.pos_ == 'PUNCT':
+                self.reset()
+
+    def finish_last_triplet_and_start_next(self):
+        self.relations_triplets.append(self.triplet)
+        next_subject = self.triplet['object']
+        self.triplet = {'subject': next_subject, 'relation': [], 'object': []}
+        self.last_token_was_propnoun = False
+
+    # call only when getting punctuation and moving to the next sentence
+    def reset(self):
+        self.triplet = {'subject': [], 'relation': [], 'object': []}
+
+    def handle_propn(self, token):
+        self.prop_noun_set.add(token.text)
+        self.last_token_was_propnoun = True
+        self.triplet['object'].append(token)
+
+    def handle_verb_or_adp(self, token):
+        self.triplet['relation'].append(token)
+
+    # this method return the output to part 1
+    def extract_valid_triplets(self):
+        result = []
+        for triplet in self.relations_triplets:
+            if self.relation_contains_verb(triplet['relation']):
+                result.append(triplet)
+        self.relations_triplets = result
+        return result
+
+
+    def relation_contains_verb(self, relation):
+        for token in relation:
+            if token.pos_ == "VERB":
+                return True
+        return False
+extractor = linear_relation_extractor()
 for token in analyzed_page:
-    print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-          token.shape_, token.is_alpha, token.is_stop)
+    extractor.step(token)
+result = extractor.extract_valid_triplets()
 
-relations_triplets=[]
-prop_noun_set=set()
-triplet={}
-first_propnoun_found=False
-verb_found=False
-prop_noun=[]
-for token in analyzed_page:
-    if token.pos_=='PROPN':
-        prop_noun.append(token.text)
-    if prop_noun and token.pos_ != 'PROPN':
-        prop_noun_full=" ".join(prop_noun)
-        prop_noun_set.add(prop_noun_full)
-        prop_noun.clear()
-        if not first_propnoun_found:
-            triplet['subject']=prop_noun_full
-            first_propnoun_found=True
-            triplet['relation']=[]
-        if first_propnoun_found and verb_found:
-            # insert relation ***
-            triplet['object'] = prop_noun_full
-            # clear all!!!!
-            first_propnoun_found = False
-            verb_found = False
-            relations_triplets.append(copy.deepcopy(triplet))
-            triplet.clear()
-        if first_propnoun_found and not verb_found:
-            first_propnoun_found = False
-            verb_found = False
-            triplet.clear()
-    if token.pos_=='VERB' and first_propnoun_found:
-        verb_found = True
-    if first_propnoun_found and (token.pos_=='VERB' or token.pos_=='ADP'):
-        triplet['relation'].append(token.text)
-
-
-# print(prop_noun_set)
-
-
-
-
-
-
+for i in range(len(analyzed_page)):
+    triplet = result[i]
+    print("-------i = "+str(i))
+    print(triplet)
